005dea2bc9894795b1d7170a465b73cf0fec56a9
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-08 14:16:49 +0600

    Update Mania.py

--- Mania.py
+++ Mania.py
@@ -12,7 +12,7 @@ import os
 from discord import Game, Embed, Color, Status, ChannelType
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = commands.Bot(command_prefix=commands.when_mentioned_or("M" ), pm_help = True)
+client = commands.Bot(command_prefix=commands.when_mentioned_or("#" ), pm_help = True)
 client.remove_command('help')
 
 GIPHY_API_KEY = "dc6zaTOxFJmzC"
